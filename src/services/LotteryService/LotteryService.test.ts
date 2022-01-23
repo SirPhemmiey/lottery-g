01481e14c499dc417614332efc7b45396749433a
Oluwafemi akinde <oluwafemiakinde@gmail.com>
2022-01-23 15:30:51 +0400

    test: add test cases

--- src/services/LotteryService/LotteryService.test.ts
+++ src/services/LotteryService/LotteryService.test.ts
@@ -61,17 +61,6 @@ describe("PUT /ticket/:id", () => {
         const updatedTicket = await service.lotteryService.updateTicketLines(n, response.id);
         expect(updatedTicket.lines.length).toBe(n+n);
     });
-});
-
-describe("GET /status/:ticket_id", () => {
-
-    it('should check status of a ticket', async () => {
-        const n = 3;
-        const response = await service.lotteryService.createLotteryTicket(n);
-        const checkedTicket = await service.lotteryService.checkTicketStatus(response.id);
-        expect(checkedTicket.ticket_status).toBe(TicketStatus.Checked);
-        expect(checkedTicket.lines.length).toBe(n);
-    });
 
     it('should fail while trying to amend a checked ticket', async () => {
         const n = 5;
@@ -87,7 +76,48 @@ describe("GET /status/:ticket_id", () => {
 
         expect(async () => {
             await service.lotteryService.updateTicketLines(n, response.id)
-        }).rejects.toThrow("You cannot amend ticket that has been checked already");
-        
+        }).rejects.toThrow("You cannot amend ticket that has been checked already"); 
     });
+});
+
+describe("GET /status/:ticket_id", () => {
+
+    it('should check status of a ticket', async () => {
+        const n = 3;
+        const response = await service.lotteryService.createLotteryTicket(n);
+        const checkedTicket = await service.lotteryService.checkTicketStatus(response.id);
+        expect(checkedTicket.ticket_status).toBe(TicketStatus.Checked);
+        expect(checkedTicket.lines.length).toBe(n);
+    });
+
+    it('should verify that outcome values are returned when ticket is checked', async () => {
+        const n = 3;
+        const response = await service.lotteryService.createLotteryTicket(n);
+        const checkedTicket = await service.lotteryService.checkTicketStatus(response.id);
+        expect(checkedTicket.ticket_status).toBe(TicketStatus.Checked);
+        expect(checkedTicket.lines.length).toBe(n);
+
+        expect(checkedTicket.lines[0].outcomeValue).toBeDefined();
+        expect(checkedTicket.lines[1].outcomeValue).toBeDefined();
+        expect(checkedTicket.lines[2].outcomeValue).toBeDefined();
+
+        const number1Array = checkedTicket.lines[0].numbers;
+        const number2Array = checkedTicket.lines[1].numbers;
+        const number3Array = checkedTicket.lines[2].numbers;
+
+        const number1Sum = number1Array.reduce((sum, value) => sum + value);
+
+       // console.log({number1Array, outcomeValue1: checkedTicket.lines[0].outcomeValue})
+        if (number1Sum === 2) {
+            expect(checkedTicket.lines[0].outcomeValue).toBe(10);
+        }
+        if (number1Array[0] === number1Array[1] && number1Array[1] === number1Array[2]) {
+            expect(checkedTicket.lines[0].outcomeValue).toBe(5);
+        } else if (number1Array[1] !== number1Array[0] && number1Array[2] !== number1Array[0]) {
+          //  expect(checkedTicket.lines[0].outcomeValue).toBe(1); number1Array: [ 0, 1, 1 ]
+        } else {
+           // expect(checkedTicket.lines[0].outcomeValue).toBe(0);
+        }
+    });
+
 });
